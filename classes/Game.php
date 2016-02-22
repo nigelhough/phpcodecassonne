35506daa97f73eb35677810b6ce70b628c635650
Tom Szuszai <tom.szuszai@workplacesystems.com>
2016-02-22 18:45:59 +0000

    created initial entry point and Game class "Bonjour Carcassonne"

---
+++ classes/Game.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class Game
+ *
+ * Pull in and execute required game elements
+ */
+class Game
+{
+    /**
+     * Run the game
+     */
+    function run()
+    {
+        echo "Bonjour Carcassonne!";
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once "classes/Game.php";
+
+$game = new Game();
+$game->run();
