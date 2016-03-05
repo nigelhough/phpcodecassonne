15d33844c504dc670dd6fa9e480068d60a5dd07a
Alex Davies <alex.davies@workplacesystems.com>
2016-03-05 09:40:55 +0000

    Add basic player: Marvin

---
+++ src/Player/Marvin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Codecassonne\Player;
+
+use Codecassonne\Map\Map;
+use Codecassonne\Tile\Tile;
+use Codecassonne\Turn\Action;
+use Codecassonne\Turn\PlayerInterface;
+
+class Marvin implements PlayerInterface
+{
+    /**
+     * Get the name of a player
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Marvin, the Paranoid Android';
+    }
+
+    /**
+     * Play a turn
+     *
+     * @param Map  $map
+     * @param Tile $tile
+     *
+     * @return Action
+     */
+    public function playTurn(Map $map, Tile $tile)
+    {
+        $playPositions = $map->getPlayablePositions();
+        shuffle($playPositions);
+
+        if (!$playPositions) {
+            throw new \Exception('Unable to find any playable positions');
+        }
+
+        // Loop over playable positions
+        foreach ($playPositions as $position) {
+            //Loop over orientations
+            for ($i = 0; $i < 4; $i++) {
+                // Rotate tile
+                try {
+                    $map->place($tile, $position);
+                    //If successfully placed, break out of rotation loop and positions loop
+                    break 2;
+                } catch (\Exception $e) {
+                    $tile->rotate();
+                }
+            }
+        }
+
+        return new Action($position, $tile->getRotation());
+    }
+}
