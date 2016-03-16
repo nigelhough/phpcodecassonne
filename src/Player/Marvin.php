da80bc7874387b1261b6a869f83382516f5a9917
Nigel Hough <nigel.hough@workplacesystems.com>
2016-03-16 20:23:41 +0000

    Refactored Marvin to be less complex

--- src/Player/Marvin.php
+++ src/Player/Marvin.php
@@ -6,6 +6,7 @@ use Codecassonne\Map\Map;
 use Codecassonne\Tile\Tile;
 use Codecassonne\Turn\Action;
 use Codecassonne\Map\Exception\InvalidTilePlacement;
+use Codecassonne\Map\Coordinate;
 
 class Marvin implements PlayerInterface
 {
@@ -20,38 +21,70 @@ class Marvin implements PlayerInterface
     }
 
     /**
+     * Get playable positions
+     *
+     * @param Map $map  Map to get playable positions on
+     *
+     * @return Coordinate[]
+     */
+    private function getPlayablePositions(Map $map)
+    {
+        // Get playable positions from the map
+        $playPositions = $map->getPlayablePositions();
+
+        // If no playable positions return early
+        if (!$playPositions) {
+            return array();
+        }
+
+        // Shuffle and return playable positions
+        shuffle($playPositions);
+        return $playPositions;
+    }
+
+    /**
+     * Gets potential positions in each orientation
+     *
+     * @param Map $map      Map to get playable positions on
+     *
+     * @return \Generator
+     */
+    public function getPotentialPosition(Map $map)
+    {
+        // Loop over playable positions
+        foreach ($this->getPlayablePositions($map) as $position) {
+            // Loop over orientations
+            for ($i = 0; $i < 4; $i++) {
+                yield $position;
+            }
+        }
+    }
+
+    /**
      * Play a turn
      *
-     * @param Map  $map
-     * @param Tile $tile
+     * @param Map  $map     Map to play turn on
+     * @param Tile $tile    Tile to use in turn
      *
      * @return Action
-     * @throws \Exception
+     *
+     * @throws Exception\NoPlayablePositions
      */
     public function playTurn(Map $map, Tile $tile)
     {
-        $playPositions = $map->getPlayablePositions();
-        shuffle($playPositions);
-
-        if (!$playPositions) {
-            throw new \Exception('Unable to find any playable positions');
-        }
-
-        // Loop over playable positions
-        foreach ($playPositions as $position) {
-            //Loop over orientations
-            for ($i = 0; $i < 4; $i++) {
+        //Loop Over potential positions in each orientation
+        foreach($this->getPotentialPosition($map) AS $position) {
+            try {
+                $map->place($tile, $position);
+                // If successfully placed, return action
+                return new Action($position, $tile->getRotation());
+            } catch (InvalidTilePlacement $e) {
                 // Rotate tile
-                try {
-                    $map->place($tile, $position);
-                    //If successfully placed, break out of rotation loop and positions loop
-                    break 2;
-                } catch (InvalidTilePlacement $e) {
-                    $tile->rotate();
-                }
+                $tile->rotate();
             }
         }
 
-        return new Action($position, $tile->getRotation());
+        // If the player hasn't found a valid action
+        throw new Exception\NoPlayablePositions('Unable to find any playable positions');
     }
 }
