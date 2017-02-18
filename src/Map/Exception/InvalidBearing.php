cb487dd1cab5547f0f19e2d1c41a999ce17eacef
Nigel Hough <nigel@nigelhough.co.uk>
2017-02-19 10:47:41 +1100

    Created custom exception for getting offset coordinate with invalid bearing

---
+++ src/Map/Exception/InvalidBearing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Codecassonne\Map\Exception;
+
+/**
+ * An exception for cases where a trying to get a coordinate offset by a bearing with an invalid bearing
+ */
+class InvalidBearing extends \Exception
+{
+}
