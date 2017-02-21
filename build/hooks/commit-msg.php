0656b6cfa8d85c23a839ea765ee5d6b7adc8b42c
Nigel Hough <nigel@nigelhough.co.uk>
2017-02-21 16:58:04 +1100

    Removed Shebang from git hook scripts

--- build/hooks/commit-msg.php
+++ build/hooks/commit-msg.php
@@ -1,8 +1,7 @@
-#!/usr/bin/env php
 <?php
-$commitMessage =  file_get_contents($argv[1]);
+$commitMessage = file_get_contents($argv[1]);
 
-if(strlen($commitMessage) <= 5) {
+if (strlen($commitMessage) <= 5) {
     echo PHP_EOL;
     echo $commitMessage . PHP_EOL;
     echo "Your commit message must be greater than 5 characters." . PHP_EOL;
