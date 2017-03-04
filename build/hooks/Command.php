e889d3dee311a06ed285a966871921c5f2c6d13d
Nigel Hough <nigel@nigelhough.co.uk>
2017-03-04 21:41:21 +1100

    Updated Pre Push Checks

---
+++ build/hooks/Command.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * A CLI Command
+ */
+class Command
+{
+    /** @var */
+    private $name;
+
+    /** @var */
+    private $command;
+
+    /** @var */
+    private $options;
+
+    /** @var */
+    private $arguments;
+
+    /**
+     * Construct a CLI Command
+     *
+     * @param string $name      Command name
+     * @param string $command   Command to execute
+     * @param array  $options   Command Options
+     * @param array  $arguments Command Arguments
+     */
+    public function __construct(string $name, string $command, array $options = [], array $arguments = [])
+    {
+        $this->name = $name;
+        $this->command = $command;
+        $this->options = $options;
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * Build the command to be executed
+     *
+     * @return string
+     */
+    private function build()
+    {
+        $command = $this->command;
+
+        foreach ($this->arguments as $argument) {
+            $command .= " $argument";
+        }
+
+        foreach ($this->options as $option => $value) {
+            $command .= " --$option";
+            if (!is_null($value)) {
+                $command .= " $value";
+            }
+        }
+
+        return $command;
+    }
+
+    /**
+     * Execute the command
+     *
+     * @return bool
+     */
+    public function execute()
+    {
+        $name = $this->name;
+        echo "Running {$name}...\n";
+        exec($this->build(), $checkOutput, $result);
+
+        if ($result !== 0) {
+            // Convert check out into readable summary format
+            $output = array_pop($checkOutput);
+            $output = array_pop($checkOutput) . "\n{$output}\n";
+            echo "{$output}Did you break something?\nAborting\n";
+            return false;
+        }
+        echo "{$name} Passed!\n";
+        return true;
+    }
+}
--- build/hooks/pre-push.php
+++ build/hooks/pre-push.php
@@ -1,43 +1,59 @@
 <?php
 declare(strict_types = 1);
 
-echo 'Checking Push ...' . PHP_EOL;
+require_once 'Command.php';
 
-if (!executeCheck('Code linting', 'vendor/bin/parallel-lint --exclude vendor .')) {
-    exit;
-}
-if (!executeCheck('Unit Tests', 'vendor/bin/phpunit -c tests/phpunit.xml --coverage-html tests/coverage')) {
-    exit;
-}
-if (!executeCheck('PHP Stan', 'vendor/bin/phpstan analyse src --no-progress --level=4')) {
-    exit;
-}
-if (!executeCheck('PHP Code Sniffer', 'vendor/bin/phpcs src')) {
-    exit;
+$lint = new Command(
+    'Code linting',
+    'vendor/bin/parallel-lint',
+    [
+        'exclude' => 'vendor',
+    ],
+    [
+        '.'
+    ]
+);
+if (!$lint->execute()) {
+    exit(1);
 }
 
-/**
- * Execute a command that runs a check on the codebase
- *
- * @param string $checkName    Name of the check on the codebase
- * @param string $checkCommand Command to run the check
- *
- * @return bool Has passed check?
- */
-function executeCheck($checkName, $checkCommand)
-{
-    echo "Running {$checkName}...\n";
-    exec($checkCommand, $checkOutput, $result);
+$unit = new Command(
+    'Unit Tests',
+    'vendor/bin/phpunit',
+    [
+        'configuration' => 'tests/phpunit.xml',
+    ]
+);
+if (!$unit->execute()) {
+    exit(1);
+}
 
-    if ($result !== 0) {
-        // Convert check out into readable summary format
-        $output = array_pop($checkOutput);
-        $output = array_pop($checkOutput) . "\n{$output}\n";
-        echo "{$output}Did you break something?\nAborting\n";
-        return false;
-    }
-    echo "{$checkName} Passed!\n";
-    return true;
+$stan = new Command(
+    'PHP Stan',
+    'vendor/bin/phpstan',
+    [
+        'no-progress' => null,
+        'level'       => '4',
+    ],
+    [
+        'analyse',
+        'src',
+    ]
+);
+if (!$stan->execute()) {
+    exit(1);
+}
+
+$sniffer = new Command(
+    'PHP Code Sniffer',
+    'vendor/bin/phpcs',
+    [],
+    [
+        'src',
+    ]
+);
+if (!$sniffer->execute()) {
+    exit(1);
 }
 
 exit(0);
